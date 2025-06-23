e395a40793da110ee6af486dbd3d435f8e97f880
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-23 12:17:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+let btn=document.querySelector("button");
+
+let a=0;
+
+btn.addEventListener("click",()=>{
+
+
+    alert(a);
+let p=document.querySelector("p");
+
+
+let b=p.innerText=a+1;
+
+a=b;
+
+p.innerText=a;
+
+
+console.log(p.innerText);
+
+
+})
